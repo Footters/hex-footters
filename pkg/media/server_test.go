168001ff03c4b9b3f60583a0d10aba1c38d2419c
David L. Carrascal <davidlcarrascal@gmail.com>
2019-04-10 15:59:10 +0200

    testing media server

--- pkg/media/server_test.go
+++ pkg/media/server_test.go
@@ -1,6 +1,12 @@
 package media_test
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+
 	"github.com/Footters/hex-footters/pkg/media"
 	"github.com/Footters/hex-footters/pkg/media/mocks"
 	httptransport "github.com/go-kit/kit/transport/http"
@@ -53,4 +59,64 @@ func (suite *MediaServerTestSuite) SetupTest() {
 		media.DecodeSetContentLiveRequest,
 		media.EncodeResponse,
 	)
+}
+
+func (suite *MediaServerTestSuite) TestGetContent() {
+	c := &media.Content{
+		URLName:     "sevillafc-betis",
+		Title:       "Sevilla FC vs Real Betis",
+		Description: "Derbi sevillano",
+		Status:      "pending",
+		Free:        1,
+		Visible:     1,
+	}
+	suite.svc.EXPECT().FindContentByID(c.ID).Return(c, nil)
+
+	url := "/contents/" + strconv.Itoa(int(c.ID))
+	r, _ := http.NewRequest("GET", url, nil)
+	w := httptest.NewRecorder()
+
+	suite.getContent.ServeHTTP(w, r)
+
+	response := w.Result()
+	suite.Equal("200 OK", response.Status)
+	defer response.Body.Close()
+
+	// res := new(media.Content)
+	// json.NewDecoder(response.Body).Decode(res)
+
+	// suite.Equal(c.URLName, result.URLName, "should be the same")
+	// suite.Equal(c.Status, result.Status, "should be the same")
+}
+func (suite *MediaServerTestSuite) TestGetAllContents() {
+
+}
+func (suite *MediaServerTestSuite) TestCreateContent() {
+	c := &media.Content{
+		URLName:     "sevillafc-betis",
+		Title:       "Sevilla FC vs Real Betis",
+		Description: "Derbi sevillano",
+		Status:      "pending",
+		Free:        1,
+		Visible:     1,
+	}
+	suite.svc.EXPECT().CreateContent(gomock.Eq(c)).Return(nil)
+
+	body, _ := json.Marshal(c)
+	r, _ := http.NewRequest("POST", "/contents", bytes.NewBuffer(body))
+	w := httptest.NewRecorder()
+
+	suite.createContent.ServeHTTP(w, r)
+
+	response := w.Result()
+	suite.Equal("200 OK", response.Status)
+
+	defer response.Body.Close()
+	// result := new(media.Content)
+	// json.NewDecoder(response.Body).Decode(result)
+
+	// suite.Equal("Register OK", result.Msg)
+}
+func (suite *MediaServerTestSuite) TestToLiveContent() {
+
 }
